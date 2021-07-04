befde573930e07a86b801c6b02ca8e907e594525
Yannick HUGUES <yannhweb@gmail.com>
2021-07-04 14:56:54 +0200

    Etape 17(test): mise en place du MakeFile

--- tests/ConferenceControllerTest.php
+++ tests/ConferenceControllerTest.php
@@ -6,25 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ConferenceControllerTest extends WebTestCase
 {
-    public function testSomething(): void
+    public function testIndex(): void
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/');
 
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Give your feedback');
-    }
-
-    public function testConferencePage()
-    {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/');
-        $this->assertCount(2, $crawler->filter('h4'));
-        $client->clickLink('View');
-        $this->assertPageTitleContains('Amsterdam');
-        $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h2', 'Amsterdam 2019');
-        $this->assertSelectorExists('div:contains("There are 1 comments")');
     }
 
     public function testCommentSubmission()
@@ -40,5 +28,17 @@ class ConferenceControllerTest extends WebTestCase
         $this->assertResponseRedirects();
         $client->followRedirect();
         $this->assertSelectorExists('div:contains("There are 2 comments")');
+    }
+
+    public function testConferencePage()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+        $this->assertCount(2, $crawler->filter('h4'));
+        $client->clickLink('View');
+        $this->assertPageTitleContains('Amsterdam');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Amsterdam 2019');
+        $this->assertSelectorExists('div:contains("There are 1 comments")');
     }
 }
